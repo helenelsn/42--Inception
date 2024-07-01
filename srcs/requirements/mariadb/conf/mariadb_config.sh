a00986b182761045ff9353fd0a07fdf97f46da33
hlesny <hlesny@student.42.fr>
2024-07-01 19:59:51 +0200

    test

--- srcs/requirements/mariadb/conf/mariadb_config.sh
+++ srcs/requirements/mariadb/conf/mariadb_config.sh
@@ -1,37 +1,12 @@
 #! /bin/bash
 
-#service mysql start;
-#
-#mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-#mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-#mysql -e "GRANT ALL PRIVILEGES ON '${SQL_DATABASE}'.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}' && FLUSH PRIVILEGES;"
-#
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-#
-#exec mysqld_safe
+service mysql start;
 
-if [ -f /is_install ]
-then 
-	echo "Database already installed"
-else
-	echo "Configuring $SQL_DATABASE ..."
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON '${SQL_DATABASE}'.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}' && FLUSH PRIVILEGES;"
 
-	#mysql_install_db
-	killall mysqld
-	mysql_install_db &> /dev/null;
-	service mysql start 2> /dev/null;
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-	#rootpassword, user, privileges ...
-	mysql -e "CREATE DATABASE $SQL_DATABASE;";
-	mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';";
-	mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';";
-	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';FLUSH PRIVILEGES;";
-	
-	#to configure it only one time
-	touch /is_install
-fi
-
-killall mysqld
-echo "database $SQL_DATABASE ready"
-mysqld --bind-address=0.0.0.0
+exec mysqld_safe
